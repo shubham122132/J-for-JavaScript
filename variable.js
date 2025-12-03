ce98faebb349076e14659da1487c60f59ea26ea9
Shubham <shubhamkumar90150@gmail.com>
2025-12-03 17:11:50 +0530

    learning variable

---
+++ variable.js
@@ -0,0 +1,12 @@
+let fullName = "shubham"
+fullName = "shubham kumar"
+ // can not redeclare the let variable in javascript
+console.log(fullName)
+
+var city = "delhi"
+var city = "noida"  // can redeclare the var variable in javascript
+console.log(city)
+
+const pi = 3.14
+// pi = 3.14159  // can not reassign the const variable in javascript
+console.log(pi)
