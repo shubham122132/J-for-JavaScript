c6f11bd227b599ec2ff8070e1ee4254711ff0fe6
Shubham <shubhamkumar90150@gmail.com>
2025-12-05 20:56:33 +0530

    table of 10 using for loop

---
+++ ObjectPractice.js
@@ -0,0 +1,7 @@
+const product = {
+    title : "Wireless Mouse",
+    rating : 4.5,
+    offer : 5,
+    price : 150
+}
+console.log(product)
---
+++ forLoop.js
@@ -0,0 +1,7 @@
+//print the table of 10 using for loop
+
+let num = 10;
+
+for(let i =1;i <= 10;i++){
+    console.log(num+" * "+i+" = "+ num*i);
+}
